26fe914b7cc0b932155cb9e985c97613b0293100
Nuwan Rajika Kumarasiri <nuwan.r.kumarasiri@gmail.com>
2025-08-14 17:03:26 -0500

    add lexer

--- src/lexer.zig
+++ src/lexer.zig
@@ -1,7 +1,37 @@
 const std = @import("std");
+
+const mem = std.mem;
+const testing = std.testing;
+const expect = testing.expect;
+const pageAllocator = std.heap.page_allocator;
 const testAllocator = std.testing.allocator;
 
-pub fn readfile(name: []const u8, alloc: std.mem.Allocator) ![]const u8 {
+pub const TokenType = enum {
+    err, // unknown token
+    eof,
+    identifier,
+    val,
+    newline,
+    comment,
+
+    fn asstr(self: TokenType) []const u8 {
+        if (self == TokenType.err) {
+            return "err";
+        } else if (self == TokenType.eof) {
+            return "eof";
+        } else if (self == TokenType.identifier) {
+            return "identifier";
+        } else if (self == TokenType.val) {
+            return "val";
+        } else if (self == TokenType.newline) {
+            return "newline";
+        } else {
+            return "unknown";
+        }
+    }
+};
+
+fn readfile(name: []const u8, alloc: std.mem.Allocator) ![]const u8 {
     const file = try std.fs.cwd().openFile(name, .{});
     defer file.close();
 
